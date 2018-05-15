31eedfa89847e7f9d90e4586197d2d31e905608f
ABN <abn.java@gmail.com>
2018-05-15 14:21:41 +0300

    start spring boot project. Study scheduling application.

---
+++ src/main/java/app/web/TreatmentScheduleApplication.java
@@ -0,0 +1,12 @@
+package app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreatmentScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreatmentScheduleApplication.class, args);
+	}
+}
